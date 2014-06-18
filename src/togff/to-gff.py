3c90452fceb2157345948c68d0a6a65f08657df4
Mitchell J Stanton-Cook <m.stantoncook@gmail.com>
2014-06-18 16:19:11 +1000

    Remove __init__ imports

--- src/togff/to-gff.py
+++ src/togff/to-gff.py
@@ -1,23 +1,30 @@
 #!/usr/bin/env python
 
-# Copyright 2013 Mitchell Stanton-Cook Licensed under the
-#     Educational Community License, Version 2.0 (the "License"); you may
-#     not use this file except in compliance with the License. You may
-#     obtain a copy of the License at
+# Copyright 2013-2014 Mitchell Stanton-Cook Licensed under the
+# Educational Community License, Version 2.0 (the "License"); you may
+# not use this file except in compliance with the License. You may
+# obtain a copy of the License at
 #
-#      http://www.osedu.org/licenses/ECL-2.0
+#     http://www.osedu.org/licenses/ECL-2.0
 #
-#     Unless required by applicable law or agreed to in writing,
-#     software distributed under the License is distributed on an "AS IS"
-#     BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
-#     or implied. See the License for the specific language governing
-#     permissions and limitations under the License.
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an "AS IS"
+# BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+# or implied. See the License for the specific language governing
+# permissions and limitations under the License.
 
 """
 Generate gff file from EMBL/Genbank for QUAST
 """
 
-import __init__ as meta
+__title__        = 'to_gff'
+__version__      = '0.1'
+__description__  = "Generate gff file from EMBL/Genbank for QUAST"
+__author__       = 'Mitchell Stanton-Cook'
+__license__      = 'ECL 2.0'
+__author_email__ = "m.stantoncook@gmail.com"
+__url__         = 'http://github.com/mscook/to_gff'
+
 
 import sys, os, traceback, argparse
 import time
@@ -25,13 +32,13 @@ import time
 from BCBio import GFF
 from Bio import SeqIO
 
-epi = "Licence: %s by %s <%s>" % (meta.__license__, 
-                                  meta.__author__,
-                                  meta.__author_email__)
-__doc__ = " %s v%s - %s (%s)" % (meta.__title__, 
-                                 meta.__version__, 
-                                 meta.__description__, 
-                                 meta.__url__)
+epi = "Licence: %s by %s <%s>" % (__license__,
+                                  __author__,
+                                  __author_email__)
+__doc__ = " %s v%s - %s (%s)" % (__title__,
+                                 __version__,
+                                 __description__,
+                                 __url__)
 
 def to_GFF(args):
     """
@@ -53,28 +60,28 @@ def to_GFF(args):
     with open(args.in_file) as fin, open(args.out_file, 'w') as fout:
         GFF.write(SeqIO.parse(fin, in_type), fout)
     if args.getfasta == True:
-        with open(args.in_file) as fin, open(fasta_out, 'w') as opt_out: 
+        with open(args.in_file) as fin, open(fasta_out, 'w') as opt_out:
             SeqIO.write(SeqIO.parse(fin, in_type), opt_out, "fasta")
 
 if __name__ == '__main__':
     try:
         start_time = time.time()
         parser = argparse.ArgumentParser(description=__doc__, epilog=epi)
-        parser.add_argument('-v', '--verbose', action='store_true', 
+        parser.add_argument('-v', '--verbose', action='store_true',
                                 default=False, help='verbose output')
-        parser.add_argument('--embl',action='store_true', 
+        parser.add_argument('--embl',action='store_true',
                                 default=False, help=('Whether we have an '
                                                 'EMBL or GenBank (default)'))
-        parser.add_argument('--getfasta',action='store_true', 
+        parser.add_argument('--getfasta',action='store_true',
                                 default=False, help=('Get a FASTA file '
                                     '(default = no)'))
-        parser.add_argument('in_file', action='store', 
+        parser.add_argument('in_file', action='store',
                                 help=('Full path to the input .embl/.gbk'))
-        parser.add_argument('out_file', action='store', 
+        parser.add_argument('out_file', action='store',
                                 help=('Full path to the output GFF'))
         parser.set_defaults(func=to_GFF)
         args = parser.parse_args()
-        if args.verbose: 
+        if args.verbose:
             print "Executing @ " + time.asctime()
         args.func(args)
         if args.verbose:
